f479e5a2a9e99617f926bdc2949f6d9c1732d610
blaise <blaise@blaise-HP-Pavilion-g7-Notebook-PC>
2014-12-18 21:49:48 +0100

    Test sur l'assignation.

--- src/tests/creationarbre/TestAssignation.java
+++ src/tests/creationarbre/TestAssignation.java
@@ -5,11 +5,24 @@ package tests.creationarbre;
 
 import arbre.Arbre;
 import arbre.GenererArbre;
+import exception.SyntaxErrorException;
 
 public class TestAssignation {
     
     public static void main(String[] args) {
-        Arbre a = GenererArbre.genererArbreSyntaxique("a := 3");
-        a.afficherArbre();
+        Arbre a;
+        try {
+            a = GenererArbre.genererArbreSyntaxique("a := 3");
+            a.afficherArbre();
+        } catch (SyntaxErrorException ex) {
+            System.out.println("Erreur de syntaxe.");
+        }
+        
+        try {
+            a = GenererArbre.genererArbreSyntaxique("a :=");
+            a.afficherArbre();
+        } catch (SyntaxErrorException ex) {
+            System.out.println("Erreur de syntaxe.");
+        }
     }
 }
