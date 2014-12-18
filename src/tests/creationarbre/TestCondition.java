2d6308716603bc954587515af6416809878c8a15
blaise <blaise@blaise-HP-Pavilion-g7-Notebook-PC>
2014-12-19 00:35:23 +0100

    Test sur la création d'arbres avec une condition if

---
+++ src/tests/creationarbre/TestCondition.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tests.creationarbre;
+
+import static tests.creationarbre.TestArbre.afficherArbreAvecCommande;
+
+/**
+ *
+ * @author blaise
+ */
+public class TestCondition {
+    
+    public static void main(String[] args) {
+        afficherArbreAvecCommande("if 2 + 3 then a");
+    }
+}
