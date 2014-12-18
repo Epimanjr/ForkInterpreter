76ff0d779c53870cc9dd81b5b5b3c828c55051d1
blaise <blaise@blaise-HP-Pavilion-g7-Notebook-PC>
2014-12-18 21:50:49 +0100

    Gestion des erreurs de syntaxe.

---
+++ src/exception/SyntaxErrorException.java
@@ -0,0 +1,15 @@
+/*
+ * Exception généré si l'utilisateur fait une erreur de syntaxe.
+ */
+package exception;
+
+public class SyntaxErrorException extends Exception {
+
+    /**
+     * Constructeur vide.
+     */
+    public SyntaxErrorException() {
+        //System.out.println("Erreur de syntaxe.");
+    }
+   
+}
