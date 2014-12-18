3a2ec0d8a36f495664b1e8cae0f53ff16c98e5d6
blaise <blaise@blaise-HP-15-Notebook-PC>
2014-12-18 17:15:58 +0100

    Ajout de la gestion des Noeuds (à la main)

---
+++ src/arbre/Noeud.java
@@ -0,0 +1,92 @@
+/*
+ * Class qui gère un Noeud.
+ */
+package arbre;
+
+import java.util.ArrayList;
+
+public class Noeud {
+
+    /**
+     * Valeur du Noeud.
+     */
+    private final String valeur;
+
+    /**
+     * La liste des Noeuds enfants.
+     */
+    private ArrayList<Noeud> fils;
+
+    /**
+     * Le Noeud père.
+     */
+    private Noeud noeudPere;
+
+    /**
+     * Construit un Noeud à partir de sa valeur (donné en paramètre)
+     *
+     * @param valeur La valeur du Noeud.
+     */
+    public Noeud(String valeur) {
+        this.valeur = valeur;
+        this.fils = new ArrayList<>();
+    }
+
+    /**
+     * Ajout d'un fils à la liste des Noeuds enfants.
+     *
+     * @param n un Noeud.
+     */
+    public void ajouterFils(Noeud n) {
+        // Ajout du lien vers le père
+        n.setNoeudPere(this);
+        // Ajout du Noeud dans la liste.
+        this.fils.add(n);
+    }
+
+    /**
+     * Retourne vrai si et seulement si le Noeud (this) est une feuille.
+     *
+     * @return vrai ou faux.
+     */
+    public boolean estUneFeuille() {
+        return this.fils.isEmpty();
+    }
+
+    /**
+     * Récupère la valeur du Noeud.
+     *
+     * @return type String
+     */
+    public String getValeur() {
+        return valeur;
+    }
+
+    /**
+     * Récupère les fils du Noeud.
+     *
+     * @return une liste de Noeuds.
+     */
+    public ArrayList<Noeud> getFils() {
+        return fils;
+    }
+
+    /**
+     * Récupère le Noeud père.
+     *
+     * @return type Noeud.
+     */
+    public Noeud getNoeudPere() {
+        return noeudPere;
+    }
+
+    /**
+     * Permet de renseigner le Noeud père.
+     *
+     * @param noeudPere type Noeud.
+     */
+    public void setNoeudPere(Noeud noeudPere) {
+        this.noeudPere = noeudPere;
+    }
+
+}
