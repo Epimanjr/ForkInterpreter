cba3f588592df01fe00de7e7c93442d379868e4e
blaise <blaise@blaise-HP-15-Notebook-PC>
2014-12-19 10:40:17 +0100

    Ajout des tests sur l'itération

---
+++ src/tests/creationarbre/TestIteration.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tests.creationarbre;
+
+import static tests.creationarbre.TestArbre.afficherArbreAvecCommande;
+
+/**
+ *
+ * @author blaise
+ */
+public class TestIteration {
+
+    public static void main(String[] args) {
+        afficherArbreAvecCommande("while 2 + 3 > 4 do 2 + 3");
+
+    }
+
+}
