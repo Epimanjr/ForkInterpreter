a7d332111e9a3c6c6dcb56a4a6dc2231753c56e2
TheMaster57 <maxime.blaise12@gmail.com>
2014-12-29 23:38:45 +0100

    Ajout d'un fichier de configuration pour l'interface.

---
+++ src/graphique/Config.java
@@ -0,0 +1,21 @@
+/*
+ * Cette classe contient toutes les variables de configuration.
+ */
+package graphique;
+
+/**
+ *
+ * @author Maxime
+ */
+public final class Config {
+    
+    /**
+     * Largeur de la fenêtre.
+     */
+    public static final int largeur = 400;
+
+    /**
+     * Hauteur de la fenêtre.
+     */
+    public static final int hauteur = 300;
+}
